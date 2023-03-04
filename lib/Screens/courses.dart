aaf2272adaf0f10f50eb45e4fa52f1aaceaf32e2
Zahraa Zahir <zahraazahiirr@gmail.com>
2023-03-04 13:10:55 +0300

    modify the orginal list of myCourses instead of creating a new list

--- lib/Screens/courses.dart
+++ lib/Screens/courses.dart
@@ -9,9 +9,6 @@ class CoursesScreen extends StatefulWidget {
 }
 
 class _CoursesScreenState extends State<CoursesScreen> {
-  var completedCourses =
-      myCourses.where((myCourses) => myCourses['percentage'] == 100).toList();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,122 +27,123 @@ class _CoursesScreenState extends State<CoursesScreen> {
   Widget getBody() {
     return ListView(children: [
       Column(
-        children: List.generate(completedCourses.length, (index) {
-          var data = completedCourses[index];
-
+        children: List.generate(myCourses.length, (index) {
           var size = MediaQuery.of(context).size;
-
-          return Padding(
-              padding: const EdgeInsets.only(
-                bottom: 20,
-              ),
-              child: Container(
-                  margin: const EdgeInsets.only(left: 10, right: 10),
-                  padding: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 19, 66, 92),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Container(
-                        child: Row(
-                          children: [
-                            Container(
-                              margin: EdgeInsets.only(right: 8),
-                              height: size.width * .17,
-                              width: size.width * .17,
-                              child: ClipRRect(
-                                child: Image.network(
-                                  data['image'],
-                                  fit: BoxFit.cover,
+          var data = myCourses[index];
+          if (data['percentage'] == 100) {
+            return Padding(
+                padding: const EdgeInsets.only(
+                  bottom: 20,
+                ),
+                child: Container(
+                    margin: const EdgeInsets.only(left: 10, right: 10),
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 19, 66, 92),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          child: Row(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: 8),
+                                height: size.width * .17,
+                                width: size.width * .17,
+                                child: ClipRRect(
+                                  child: Image.network(
+                                    data['image'],
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              width: 3,
-                            ),
-                            Expanded(
-                                child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  data['title'],
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w700,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 5,
-                                ),
-                                Align(
-                                  alignment: Alignment.centerLeft,
-                                  child: Text(
-                                    'Instructor: ${data['instructor_name']}',
+                              const SizedBox(
+                                width: 3,
+                              ),
+                              Expanded(
+                                  child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    data['title'],
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
                                     style: const TextStyle(
-                                      color: Colors.grey,
-                                      fontSize: 11,
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w700,
                                     ),
                                   ),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                      'Instructor: ${data['instructor_name']}',
+                                      style: const TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 11,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                                child: Stack(
+                              alignment: Alignment.centerLeft,
+                              children: [
+                                Container(
+                                  width: size.width,
+                                  height: 5,
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.1),
+                                      borderRadius: BorderRadius.circular(100)),
+                                ),
+                                Container(
+                                  width: data['percentage'] * 2.9,
+                                  height: 7,
+                                  decoration: BoxDecoration(
+                                      color: Colors.amber.withOpacity(0.5),
+                                      borderRadius: BorderRadius.circular(100),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.amber.withOpacity(0.5),
+                                          blurRadius: 6,
+                                          offset: const Offset(0, 3),
+                                        )
+                                      ]),
                                 )
                               ],
                             )),
-                          ],
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                              child: Stack(
-                            alignment: Alignment.centerLeft,
-                            children: [
-                              Container(
-                                width: size.width,
-                                height: 5,
-                                decoration: BoxDecoration(
-                                    color: Colors.white.withOpacity(0.1),
-                                    borderRadius: BorderRadius.circular(100)),
-                              ),
-                              Container(
-                                width: data['percentage'] * 2.9,
-                                height: 7,
-                                decoration: BoxDecoration(
-                                    color: Colors.amber.withOpacity(0.5),
-                                    borderRadius: BorderRadius.circular(100),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.amber.withOpacity(0.5),
-                                        blurRadius: 6,
-                                        offset: const Offset(0, 3),
-                                      )
-                                    ]),
-                              )
-                            ],
-                          )),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Container(
-                            width: 50,
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              '${data['percentage']}%',
-                              style: const TextStyle(
-                                  color: Colors.amber,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w700),
+                            const SizedBox(
+                              width: 10,
                             ),
-                          )
-                        ],
-                      )
-                    ],
-                  )));
+                            Container(
+                              width: 50,
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                '${data['percentage']}%',
+                                style: const TextStyle(
+                                    color: Colors.amber,
+                                    fontSize: 13,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    )));
+          }
+          return const SizedBox();
         }),
       ),
     ]);
